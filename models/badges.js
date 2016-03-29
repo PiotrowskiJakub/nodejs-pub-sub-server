83f57cdbc6afb45f9e40ab8f02a091f6b6173f0f
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2016-03-29 02:56:01 +0200

    Simple Pub/Sub server

---
+++ models/badges.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var redis = require('../lib/redis');
+var broadcast = require('../lib/broadcast');
+
+/**
+ *  Save badges to database
+ *  @param {Array} badges
+ *  @param (Function) callback
+ */
+exports.save = function(badges, callback) {
+  if (!badges.length) return callback(null, null);
+  var badge = badges.pop();
+  redis.lpush('badges', JSON.stringify(badge), function(err) {
+      if (err) return callback(err, null);
+      exports.save(badges, callback);
+  });
+};
+
+/**
+ *  Send out badges to the broadcaster
+ *  @param {Array} badges
+ *  @param (Function) callback
+ */
+exports.send = function(badges, callback) {
+  badges.forEach(broadcast.send);
+  callback(null, null);
+};
+
+/**
+ *  Get badges from redis
+ *  @param {Function} callback
+ */
+exports.get = function(callback) {
+  redis.lrange('badges', 0, -1, function(err, data) {
+    if (err) return callback(err, null);
+    callback(null, data.map(JSON.parse));
+  });
+};
+
+/**
+ *  Trim down the redis list
+ */
+exports.trim = function() {
+  redis.ltrim('badges', 0, 9);
+};
